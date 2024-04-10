bb316746dd0e774f86646eae17036ddfdf323e73
psnapyeditor <info.psnapy@gmail.com>
2024-04-10 14:08:29 +0300

    updated get feed to pull random posts

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -107,7 +107,7 @@ const Actions = ({ post }) => {
 
 	return (
 		<Flex flexDirection='column'>
-			<Flex gap={3} my={1} onClick={(e) => e.preventDefault()}>
+			<Flex gap={3} my={1} mt={-1} onClick={(e) => e.preventDefault()}>
 				<svg
 					aria-label='Like'
 					color={liked ? "#FF007F" : ""}
