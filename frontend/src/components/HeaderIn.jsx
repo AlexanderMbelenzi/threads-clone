874deb6bf6bccc23052e9d6b239e7956e04bc625
psnapyeditor <info.psnapy@gmail.com>
2024-04-12 17:56:30 +0300

    updated get feed to pull random posts

--- frontend/src/components/HeaderIn.jsx
+++ frontend/src/components/HeaderIn.jsx
@@ -1,84 +1,62 @@
 import React from "react";
-import { Box, Flex, Image, Link, InputLeftElement, Input, InputGroup,  useColorMode } from "@chakra-ui/react";
+import { Box, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
 import authScreenAtom from "../atoms/authAtom";
 import { Link as RouterLink } from "react-router-dom";
 import { SearchIcon } from "@chakra-ui/icons";
 
-
-
 const HeaderIn = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     const user = useRecoilValue(userAtom);
     const setAuthScreen = useSetRecoilState(authScreenAtom);
 
+    return (
+        <Box
+            pl="20%"
+            top="0"
+            py={4}
+            bg={colorMode === "dark" ? "blackAlpha.800" : "whiteAlpha.800"}
+            zIndex="999"
+        
+            width="100%"
+        >
+            <Flex justifyContent="space-between" alignItems="center">
+                {!user && (
+                    <>
+                        <Link as={RouterLink} to="/auth" onClick={() => setAuthScreen("login")}>
+                            Login
+                        </Link>
+                        <Link as={RouterLink} to="/auth" onClick={() => setAuthScreen("signup")}>
+                            Sign up
+                        </Link>
+                    </>
+                )}
 
-  return (
-    <Box
-      px="20%"
-      top="0"
-     
-      justifyContent="center"
-      py={4}
-      bg={colorMode === "dark" ? "blackAlpha.800" : "whiteAlpha.800"}
-      zIndex="999"
-     
-     
-    >
-      <Flex justifyContent="space-between">
-        {!user && (
-          <>
-            <Link as={RouterLink} to="/auth" onClick={() => setAuthScreen("login")}>
-              Login
-            </Link>
-            <Link as={RouterLink} to="/auth" onClick={() => setAuthScreen("signup")}>
-              Sign up
-            </Link>
-          </>
-        )}
+                {user && (
+                    <>
+                        <Link as={RouterLink} to="/">
+                            For you
+                        </Link>
 
-        {user && (
-          <Flex alignItems="center" justifyContent="space-between"     gap={4}>
-            <Link as={RouterLink} to="/">
-              For you
-            </Link>
+                        <Flex justifyContent="center" alignItems="center">
+                            <Image
+                                cursor="pointer"
+                                alt="logo"
+                                w={6}
+                                src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+                                onClick={toggleColorMode}
+                            />
+                        </Flex>
 
-            <Image
-                
-         ml={12}
-         justifyContent="space-between"
-          cursor="pointer"
-          alt="logo"
-          w={6}
-          src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-          onClick={toggleColorMode}
-          
-        />
-
-
-            <Link as={RouterLink} to="/home2"  justifyContent="space-between"  ml={12}         
-       // Add margin to the logo to increase space
-  > 
-              Following
-            </Link>
-
-          </Flex>
-        )}
-
-       
-
-
-
-      </Flex>
-
-
-	
-  
-
-
-    </Box>
-  );
+                        <Link as={RouterLink} to="/home2">
+                            Following
+                        </Link>
+                    </>
+                )}
+            </Flex>
+        </Box>
+    );
 };
 
 export default HeaderIn;
