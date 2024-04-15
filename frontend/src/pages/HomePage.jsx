97a8b7bb26b1ae26096505be2d446e2027514309
psnapyeditor <info.psnapy@gmail.com>
2024-04-15 17:04:57 +0300

    updated get feed to pull random posts

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,27 +13,27 @@ const HomePage = () => {
 	const [posts, setPosts] = useRecoilState(postsAtom);
 	const [loading, setLoading] = useState(true);
 	const showToast = useShowToast();
-	useEffect(() => {
-		const getFeedPosts2 = async () => {
-			setLoading(true);
-			setPosts([]);
-			try {
-				const res = await fetch("/api/posts/feed2");
-				const data = await res.json();
-				if (data.error) {
-					showToast("Error", data.error, "error");
-					return;
-				}
-				console.log(data);
-				setPosts(data);
-			} catch (error) {
-				showToast("Error", error.message, "error");
-			} finally {
-				setLoading(false);
-			}
-		};
-		getFeedPosts2();
-	}, [showToast, setPosts]);
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      setPosts([]);
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast, setPosts]);
 
 
   return (
